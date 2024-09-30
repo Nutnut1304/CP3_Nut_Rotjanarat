1db0eca3032f036aeb7c2c76aecd5492775a8dbe
Nutnut1304 <rotjanaratn@gmail.com>
2024-09-30 18:47:00 +0700

    Create Exercise4_Nut.R.py

---
+++ Exercise4_Nut.R.py
@@ -0,0 +1,13 @@
+FE = 80
+GB = 91
+ICS = 75
+CP = 70
+print("   Your", "Score")
+print("-----------------")
+print("Foundation English","คะแนนที่ได้ : ", FE , "/100")
+print("")
+print("General Busines","คะแนนที่ได้ : ", GB , "/100")
+print("")
+print("Introduction to Computer Systems","คะแนนที่ได้ : ", ICS , "/100")
+print("")
+print("Computer Programming","คะแนนที่ได้ : ", CP , "/100")
